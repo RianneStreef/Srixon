cbf065cf15a00d4e614a3e81058d2d88469da9aa
RianneStreef <riannestreef@gmail.com>
2021-03-05 11:17:57 +0100

    tour staff bag changed

--- sw.js
+++ sw.js
@@ -1,7 +1,14 @@
 console.log("service worker running");
 
-var cacheName = "/srixon-v3";
-var contentToCache = ["../index.html", "../categories.html"];
+var cacheName = "/srixon-v11";
+var contentToCache = [
+  "../index.html",
+  "../categories.html",
+  "../categories/golf-bags.html",
+  "../products/golf-bags/srixon-tour-staff.html",
+  "../products/golf-bags/tour-cart.html",
+  "../products/golf-bags/tour-stand.html",
+];
 
 self.addEventListener("install", (e) => {
   console.log("[Service Worker] Install");
