603d4f4befc43f72a8a8e92c9bd1db6814f8ce96
RianneStreef <riannestreef@gmail.com>
2021-03-05 16:52:08 +0100

    skip waiting

--- sw.js
+++ sw.js
@@ -1,16 +1,18 @@
 console.log("service worker running");
 
-var cacheName = "/srixon-v11";
+var cacheName = "/srixon-v12";
 var contentToCache = [
   "../index.html",
   "../categories.html",
-  "../categories/golf-bags.html",
-  "../products/golf-bags/srixon-tour-staff.html",
-  "../products/golf-bags/tour-cart.html",
-  "../products/golf-bags/tour-stand.html",
+  // "../categories/golf-bags.html",
+  // "../products/golf-bags/srixon-tour-staff.html",
+  // "../products/golf-bags/tour-cart.html",
+  // "../products/golf-bags/tour-stand.html",
 ];
 
 self.addEventListener("install", (e) => {
+  self.skipWaiting();
+
   console.log("[Service Worker] Install");
   e.waitUntil(
     caches.open(cacheName).then((cache) => {
