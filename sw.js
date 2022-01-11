49ae19ad60bc9072e3054fe0a34d7c353e65e3b3
Rianne Streef <56265374+RianneStreef@users.noreply.github.com>
2022-01-11 15:54:52 +0100

    hi brid glove deleted

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 console.log("service worker running");
 
-var cacheName = "/srixon-v5.9";
+var cacheName = "/srixon-v5.10";
 var contentToCache = [
   // "../index.html",
   // "../categories.html",
